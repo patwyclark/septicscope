ef150ed0f6e3ad7b86821d68b115e2a0578a4c83
patwyclark <patwyclark@gmail.com>
2026-08-31 12:03:50 -0500

    Activate septic maintenance checklist guide

--- usercustomize.py
+++ usercustomize.py
@@ -1,3 +1,4 @@
 """SepticScope user build finalizers."""
 from usercustomize_core import *  # noqa: F401,F403
 import septic_safe_guide  # noqa: F401,E402
+import septic_maintenance_checklist  # noqa: F401,E402
